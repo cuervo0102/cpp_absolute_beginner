d60f8c68b0ce2bf499c3be4a7ff6b962cb12ace6
esjfalcon <essajioussama1@gmail.com>
2024-10-17 08:49:15 +0100

    built-in function for strings

---
+++ builtin_string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main(){
+    //getline write sentence with spaces
+    std::string name;
+    std:: cout << "Enter your name: "  <<;
+    std :: getline(std::cin, name);
+
+    //length
+    std::cout << (name.length() > 9 ? "hello " + name : "hey " + name) << std::endl;
+ 
+   //name.clear()
+   //name.empty()
+   //name.append("")
+   //name.at(0) index of returned letter
+   //name.insert(0, "")
+   //name.find(" ")
+   //name.erase(0, 3)
+
+}
